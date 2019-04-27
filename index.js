40aba47ed5fcc2444f3e72d78c0977fa3d6d3d04
Koua Thao <kouathao09@gmail.com>
2019-04-27 14:26:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req, res) {
+  //     console.log(req.body.crypto);
+  var crypto = req.body.crypto;
+  var fiat = req.body.fait;
+  var amount = req.body.amount;
+
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+  request(options, function(error, response, body) {
+    var data = JSON.parse(body);
+
+    var price = data.price;
+
+    var currentDate = data.time;
+
+    res.write("<p>The current date is " + currentDate + "</p>");
+
+    res.write(
+      "<h1>" +
+        amount +
+        " " +
+        crypto +
+        " is currently worth " +
+        price +
+        " " +
+        fiat +
+        "</h1>"
+    );
+
+    res.send();
+  });
+});
+app.listen(3000, function() {
+  console.log("Sever is running on port 3000.");
+});
